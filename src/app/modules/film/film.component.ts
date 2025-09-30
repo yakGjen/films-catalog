bd931f9b0af1588ebc78ce15e2ca83cefc881505
Евгений Якимчук <yak.gjen@gmail.com>
2025-09-30 19:30:46 +0300

    feat: card component

---
+++ src/app/modules/film/film.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { FilmInterface } from './film.model';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-film.component',
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './film.component.html',
+  styleUrl: './film.component.scss'
+})
+export class FilmComponent {
+  @Input() film!: FilmInterface;
+
+  like(trig: boolean): void {}
+}
---
+++ src/app/modules/film/film.model.ts
@@ -0,0 +1,7 @@
+export interface FilmInterface {
+  id: number;
+  title: string;
+  description: string;
+  premiereDate: Date;
+  picture: string | null;
+}
