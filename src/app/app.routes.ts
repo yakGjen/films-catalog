b4cf5203635ecd19fbbcaa8c8eaece8fcd3738db
Евгений Якимчук <yak.gjen@gmail.com>
2025-09-30 18:23:26 +0300

    feat: first routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { startGuard } from './guards/start.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    canActivate: [startGuard],
+    pathMatch: 'full',
+    component: AuthComponent
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  },
+  {
+    path: 'films',
+    // canActivate: [authGuard],
+    loadComponent: () =>
+      import('./modules/films-catalog/films-catalog.component').then(m => m.FilmsCatalogComponent)
+  },
+];
